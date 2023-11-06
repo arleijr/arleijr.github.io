7ac92dd2499a6afa8feabb47b993b291b3f33c68
killianzin <arley.jr121@gmail.com>
2023-11-06 13:43:41 -0300

    portifolio

---
+++ js/script.js
@@ -0,0 +1,25 @@
+function scrollToSection(sectionId) {
+    document.getElementById(sectionId).scrollIntoView({
+        behavior: 'smooth'
+    });
+}
+function changeText(newText) {
+    document.getElementById("text").innerHTML = newText;
+}
+
+
+var swiper = new Swiper(".slide-content", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 1,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    },
+    navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+    },
+});
